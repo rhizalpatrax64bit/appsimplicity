95b049f00258a6570bdcb18e21f3bbbdda3e2a9c
javierpitalua <javierpitalua@0feab27c-6cda-11de-9e5e-bf5a4949f761>
2009-10-11 14:50:06 +0000

    Initial load

---
+++ Version0.5/Core/AppSimplicityV0.5/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("AppSimplicityV0.5")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("VMM")> 
+<Assembly: AssemblyProduct("AppSimplicityV0.5")> 
+<Assembly: AssemblyCopyright("Copyright © VMM 2009")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("aee8e6db-631c-49f8-8204-27bd8199a5a6")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
