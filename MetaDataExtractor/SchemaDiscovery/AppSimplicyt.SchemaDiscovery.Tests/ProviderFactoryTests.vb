b4d73bff929ea56d7e1c2d41433406b83c978069
javierpitalua@gmail.com <javierpitalua@gmail.com@0feab27c-6cda-11de-9e5e-bf5a4949f761>
2012-07-18 05:25:50 +0000

    Included SQLServer metadata provider implementation

--- MetaDataExtractor/SchemaDiscovery/AppSimplicyt.SchemaDiscovery.Tests/ProviderFactoryTests.vb
+++ MetaDataExtractor/SchemaDiscovery/AppSimplicyt.SchemaDiscovery.Tests/ProviderFactoryTests.vb
@@ -7,5 +7,7 @@ Public Class ProviderFactoryTests
     Public Sub TestProviderInstance()
         Dim lCS As System.Configuration.ConnectionStringSettings = System.Configuration.ConfigurationManager.ConnectionStrings("SomeERP")
         Assert.IsNotNull(AppSimplicity.SchemaDiscovery.Engine.MetaDataProviderFactory.GetInstance(lCS.ProviderName))
+
+
     End Sub
 End Class
